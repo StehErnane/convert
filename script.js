523bcbbb024d3fbb36c9a291f033748db1cf93c1
Stefanie Ernane <stefanieosernane@gmail.com>
2025-07-23 22:27:27 -0300

    Exibindo o footer

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const GBP = 6.08
 const form = document.querySelector("form")
 const amount = document.getElementById("amount")
 const currency = document.getElementById("currency")
+const footer = document.querySelector("main footer")
 
 // Manipulando o input amount para receber somente numeros
 amount.addEventListener("input" , () => {
@@ -34,5 +35,13 @@ form.onsubmit = (event) => {
 
 // Função p/ converter a moeda
 function convertCurrency(amount, price, symbol){
-
+  try{
+    // Aplica a classe que exibe o footer para mostrar 0 resultado
+    footer.classList.add("show-result")
+  } catch (error) {
+    console.log(error)
+    // remove a classe que exibe o footer que mostra o resultado
+    footer.classList.remove("show-result")
+    alert("Não foi possível converter, tente novamente mais tarde !!!")
+  }
 }
